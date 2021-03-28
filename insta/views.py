479357f4fe38097ab256605a472c8325dba42668
samfan01 <dsamfan@gmail.com>
2021-03-28 22:19:56 +0300

    created the base.html file for template inheritance

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    template_name='home.html',
+    title = 'My Own Instagram'
+    
+    return render(request,template_name,{'title':title})
