dd7a7fb474a699174cf7d9349ec517434a34dfd8
samfan01 <dsamfan@gmail.com>
2021-03-29 01:31:19 +0300

    created the image model class

--- insta/models.py
+++ insta/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class 
+class Image(models.Model):
+    image_name = models.CharField(max_length = 60)
+    image_caption = models.TextField()
+    image = models.ImageField(upload_to = 'insta/')
+    post_date = models.DateTimeField(auto_now_add = True)
