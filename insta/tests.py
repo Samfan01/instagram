037a4030fc307458bcd85845ec7fcddeb18731d0
samfan01 <dsamfan@gmail.com>
2021-04-11 12:45:05 +0300

    added the profile test case to test methods on the profile class

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    '''
+    Test case to test instances of the profile class
+    '''
+    def setUp(self):
+        self.samfan = Profile(profile_pic='url',first_name='Samfan',last_name='Douglas',bio='I am a christian')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.samfan,Profile ))
